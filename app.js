505699b0735f2d03e93e513d9b7e2c8ab573fad9
root <root@zenbership126.harishfysx.bpm>
2017-06-15 22:54:53 -0400

    Template for node-bootstrap-mongo

--- app.js
+++ app.js
@@ -11,9 +11,12 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/loginapp');
-var db = mongoose.connection;
+//mongoose.connect('mongodb://localhost/loginapp');
 
+//local requires
+require('./config/config');
+var mongoose = require('./db/mongoose');
+var db = mongoose.connection;
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -75,13 +78,13 @@ app.use(function (req, res, next) {
 });
 
 
-
+//routes
 app.use('/', routes);
 app.use('/users', users);
 
 // Set Port
-app.set('port', (process.env.PORT || 3000));
+//app.set('port', (process.env.PORT || 3000));
 
-app.listen(app.get('port'), function(){
-	console.log('Server started on port '+app.get('port'));
+app.listen(process.env.PORT, function(){
+	console.log(`Example app listening on port ${process.env.PORT}!`)
 });
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var pass = require('./../middleware/pass');
 
 // Get Homepage
-router.get('/', ensureAuthenticated, function(req, res){
+router.get('/', pass.ensureAuthenticated, function(req, res){
 	res.render('index');
 });
-
-function ensureAuthenticated(req, res, next){
-	if(req.isAuthenticated()){
-		return next();
-	} else {
-		//req.flash('error_msg','You are not logged in');
-		res.redirect('/users/login');
-	}
-}
 
 module.exports = router;
